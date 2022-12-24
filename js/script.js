b09a58b2e21aae5e93c58ce2b04538c6c6c5ce82
Таня Семещук <121348380+TaniiaSeme4uk@users.noreply.github.com>
2022-12-24 16:50:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+$('#title-block').click(function(){
+    $('#sub-block').toggle(1000);
+});
+
+
+/*
+$('#title-block').click(function(){
+    $('#sub-block').show(1000);
+});
+$('#title-block').click(function(){
+    $('#sub-block').hide(1000);
+});
+
+
+
+onclick="alert('правильна відповідь: 8')"
+*/
